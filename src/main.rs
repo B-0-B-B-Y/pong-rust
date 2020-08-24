99abf739e4e421fee2dd0d4a6d27467481dc43c0
Bob Naydenov <bob.naydenov@hotmail.com>
2020-08-24 23:44:15 +0100

    Add movements and collisions to paddles and top/bottom of screen

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,18 @@ fn main() -> amethyst::Result<()> {
     let game_data = GameDataBuilder::default()
         .with_bundle(TransformBundle::new())?
         .with_bundle(input_bundle)?
-        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
         .with_bundle(RenderingBundle::<DefaultBackend>::new()
-            .with_plugin(
-                RenderToWindow::from_config_path(display_config_path)?
-                    .with_clear([0.0, 0.0, 0.0, 0.1]),
-            )
-            .with_plugin(RenderFlat2D::default()),
-        )?;
+        .with_plugin(
+            RenderToWindow::from_config_path(display_config_path)?
+            .with_clear([0.0, 0.0, 0.0, 0.1]),
+        )
+        .with_plugin(RenderFlat2D::default()),
+        )?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
+        .with(systems::MoveBallsSystem, "ball_system", &[])
+        .with(systems::BounceSystem, "collision_system", &["paddle_system", "ball_system"]);
 
-    let mut game = Application::new(assets_dir, Pong, game_data)?;
+    let mut game = Application::new(assets_dir, Pong::default(), game_data)?;
     
     game.run();
     
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,3 +1,9 @@
-pub use self::paddle::PaddleSystem;
-
 mod paddle;
+mod move_balls;
+mod bounce;
+
+pub use self::{
+  paddle::PaddleSystem,
+  move_balls::MoveBallsSystem,
+  bounce::BounceSystem,
+};
